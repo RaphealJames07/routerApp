130085bc8549cccfa3144c071755ff56625a646c
RaphealJames07 <123210977+RaphealJames07@users.noreply.github.com>
2025-02-10 17:55:58 +0100

    stress

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -2,10 +2,9 @@
 import {Navigate} from "react-router";
 
 const PrivateRoute = ({element, children}) => {
-    // const user = localStorage.getItem("userData")
-    const isLoggedIn = true;
-    console.log(isLoggedIn);
-    return isLoggedIn ===true ? (
+    const user = JSON.parse(localStorage.getItem("userData"))
+    console.log(user)
+    return user?.token ? (
         children ? (
             <>{children}</>
         ) : (
